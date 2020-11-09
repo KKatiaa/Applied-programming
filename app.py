644063236bb93b76e13d95cdaac8a88a4f6139e0
KKatiaa <53976746+KKatiaa@users.noreply.github.com>
2020-11-09 17:48:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from wsgiref.simple_server import make_server
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api/v1/hello-world-11')
 def hello_world():
-    return 'Hello, World №11!'
+    return 'Hello World №11!'
+
+server = make_server('', 8000, app)
+print('http://127.0.0.1:8000/api/v1/hello-world-11')
+server.serve_forever()
