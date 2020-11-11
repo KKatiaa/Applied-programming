b16b8fd6234e640bd121e94519cca8425ee761d9
KKatiaa <53976746+KKatiaa@users.noreply.github.com>
2020-11-11 16:52:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World №11!'
 
-server = make_server('', 8000, app)
-print('http://127.0.0.1:8000/api/v1/hello-world-11')
-server.serve_forever()
+with make_server('', 8000, app) as server:
+    print('http://127.0.0.1:8000/api/v1/hello-world-11')
+    server.serve_forever()
